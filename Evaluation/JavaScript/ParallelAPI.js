bda1140d8ae29908b4fbab77e21285fe046943f2
akshay Poonia <akshay12345poonia@gmail.com>
2026-01-07 14:56:31 +0530

    eval

---
+++ Evaluation/JavaScript/ParallelAPI.js
@@ -0,0 +1,39 @@
+async function fetchPostWithComments(){
+    try {
+        const [postsResponse, commentsResponse] = await Promise.all([
+            fetch('https://jsonplaceholder.typicode.com/posts'),
+            fetch('https://jsonplaceholder.typicode.com/comments')
+        ]);
+
+        const posts = await postsResponse.json();
+        const comments = await commentsResponse.json();
+
+        const postConmmentData = {};
+        comments.forEach(comment => {
+            const postId = comment.postId;
+            if (!postConmmentData[postId]) {
+                postConmmentData[postId] = {
+                    count: 0,
+                    firstCommentEmail: comment.email
+                };
+            }
+            postConmmentData[postId].count++;
+        });
+        const mergedData = posts.map(post => {
+            const commentData = postConmmentData[post.id] || { count: 0 };
+            return {
+                postId: post.id,
+                title: post.title,
+                commentCount: commentData.count,
+                firstCommentEmail: commentData.firstCommentEmail || null
+            }
+        });
+        return mergedData;
+    }
+    catch (error) {
+        console.error('Error fetching data:', error);
+        return;
+    }
+}
+
+fetchPostWithComments().then(data => console.log(data));
