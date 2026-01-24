451196c57d8890f1c9f7e2d10ee92e40e4086490
akshay Poonia <akshay12345poonia@gmail.com>
2026-01-24 20:53:11 +0530

    post-feed-app

--- Evaluation/React/post-feed-app/src/components/CommentList.jsx
+++ Evaluation/React/post-feed-app/src/components/CommentList.jsx
@@ -1,14 +1,31 @@
-import React from "react";
-import "./styles.css";
-const commentList = ({comments}) => {
-    return (
-        <div className="comment-list">
-            <h3 className="comments-title">
-                comments ({comments.length})
-            </h3>
-            {comments.map((comment) => (
-                
-            ))}
+import React from 'react';
+import './styles.css';
+
+const CommentList = ({ comments }) => {
+  return (
+    <div className="comment-list">
+      <h3 className="comments-title">
+        Comments ({comments.length})
+      </h3>
+      {comments.map((comment) => (
+        <div key={comment.id} className="comment-item">
+          <div className="comment-header">
+            <span className="comment-user-icon">👤</span>
+            <div className="comment-user-info">
+              <h4 className="comment-name">{comment.name}</h4>
+              <div className="comment-email">
+                <span className="email-icon">📧</span>
+                <span>{comment.email}</span>
+              </div>
+            </div>
+          </div>
+          <div className="comment-body">
+            <p>{comment.body}</p>
+          </div>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
+
+export default CommentList;
